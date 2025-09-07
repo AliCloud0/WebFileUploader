681f3c5f2a154b33d52ca47324625e7f8af5faef
AliCloud0 <alixbig856@gmail.com>
2025-09-07 04:10:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const validationMiddleware = require('./middleware/validationMiddleware');
 
 const app = express();
 app.use(express.json());
-app.use(i18n.init); // برای چندزبانه
-app.use(validationMiddleware); // امنیت
+app.use(i18n.init);
+app.use(validationMiddleware);
 
-// مسیرهای استاتیک
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API routes
@@ -20,10 +19,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/admin', adminRoutes);
 
-// صفحه اصلی
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-// همگام‌سازی دیتابیس
 sequelize.sync({ force: false }).then(() => {
   app.listen(3000, () => console.log('Server running on port 3000'));
 });
